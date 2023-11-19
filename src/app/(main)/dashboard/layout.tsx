269fb94ac843dec1086057b13da19fd6500232f9
Rohan Tanwar <rohantanwwar0809@gmail.com>
2023-11-20 00:19:53 +0530

    📦 NEW: signup with email verify , middleware for seesion

---
+++ src/app/(main)/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+interface LayoutProps {
+  children: React.ReactNode;
+  params: any;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, params }) => {
+  return <main className="flex overflow-hidden h-screen">{children}</main>;
+};
+export default Layout;
