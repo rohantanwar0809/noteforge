8f594f8c5ce53cdca9e1fdc64983d5ed83c93673
Rohan Tanwar <rohantanwar0809@gmail.com>
2024-01-20 23:42:09 +0530

    📦 NEW: dashboard setup

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,4 +1,38 @@
-const Page = () => {
-  return <div>Page</div>;
+import DashboardSetup from "@/components/dashboard-setup/dashboard-setup";
+import db from "@/lib/supabase/db";
+import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({
+    cookies,
+  });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  if (subscriptionError) return;
+
+  if (!workspace)
+    return (
+      <div className="bg-background w-screen h-screen flex justify-center items-center">
+        <DashboardSetup user={user} subscriptions={null} />
+      </div>
+    );
+
+  redirect(`/dashboard/${workspace.id}`);
+
+  return <div>DashboardPage</div>;
 };
-export default Page;
+export default DashboardPage;
